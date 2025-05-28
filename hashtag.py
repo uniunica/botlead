437c71fd28bd521f26f959b1ee46a77b2211c460
UniUnica <felipe.silva@uniunica.edu.br>
2025-05-28 09:02:39 -0300

    Atualizações

--- hashtag.py
+++ hashtag.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import time
 import random
 import csv
 import os
+import logging
 
 # Configurar navegador
 options = Options()
